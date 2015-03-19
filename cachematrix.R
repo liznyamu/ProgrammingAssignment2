e0af2506112d6bb6ecbd3b56c88b477b625aa2ab
Elizabeth Nyamu <liznyamu@gmail.com>
2015-03-19 18:54:06 +0300

    add comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -38,7 +38,7 @@ cacheSolve <- function(cacheInvs,x, ...) {
         
         ## if cached invs is not null and matrix has not changed
         ## return cached invs
-        if(!is.null(invs) & matequal(cachedX, x)){
+        if(!is.null(invs) & identical(cachedX, x)){
              return  invs
         }
         else {
@@ -49,8 +49,3 @@ cacheSolve <- function(cacheInvs,x, ...) {
         
         cacheInvs$getInverse()
 }
-
-## function copied from 
-## https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html
-matequal <- function(x, y)
-        is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
